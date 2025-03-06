d95e635db349f4c01005d4581781d222bfb6fbe6
ramrajb1096 <rrnayak2004@gmail.com>
2025-03-07 02:42:58 +0530

    cors

--- app/routes/sentiment.py
+++ app/routes/sentiment.py
@@ -7,6 +7,6 @@ router = APIRouter()
 async def fetch_sentiment(stock_name: str):
     """API Endpoint to get sentiment analysis for a stock"""
     result = get_stock_sentiment(stock_name)
-    if not result:
-        raise HTTPException(status_code=404, detail="No news articles found or API limit reached.")
+    # if not result:
+    #     raise HTTPException(status_code=404, detail="No news articles found or API limit reached.")
     return {"stock": stock_name, "analysis": result}
